a53b5ef551fe8697a144af022f6bfacda378e2ec
dangtrungdev <dangtrungdev113999@gmail.com>
2020-04-01 16:18:56 +0700

    FlatList

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [persion, setPersion] = useState([
-    { name: 'trung', key: 1 },
-    { name: 'trung', key: 2 },
-    { name: 'trung', key: 3 },
-    { name: 'trung', key: 4 },
-    { name: 'trung', key: 5 },
-    { name: 'trung', key: 6 },
-    { name: 'trung', key: 7 },
+    { name: 'trung', id: "1" },
+    { name: 'trung', id: '2' },
+    { name: 'trung', id: '3' },
+    { name: 'trung', id: '4' },
+    { name: 'trung', id: '5' },
+    { name: 'trung', id: '6' },
+    { name: 'trung', id: '7' },
   ]);
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {persion.map(item => (
-          <View key={item.key} >
-            <Text style={styles.text} >{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <FlatList
+        data={persion}
+        keyExtractor={item => item.id}
+        horizontal={false}
+        renderItem={({item}) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+        numColumns={2}
+      />
     </View>
   );
 }
@@ -34,10 +36,11 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  text: {
+  item: {
     fontSize: 20,
     fontWeight: 'bold',
     marginVertical: 40,
+    marginHorizontal: 10,
     padding: 60,
     backgroundColor: 'pink'
   }
